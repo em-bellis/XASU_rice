56c8a2d92c4e7745332130400be06a04360a9a77
Emily Bellis <emilysbellis@gmail.com>
2020-11-05 10:36:49 -0600

    Create process_VIs.R

---
+++ 1_process_TIF_layers/process_VIs.R
@@ -0,0 +1,15 @@
+process_VIs <- function() {
+  # This function processes an input raster to have the same extent, crs, and resolution as an example raster
+  # Vegetation indices are also processed to set appropriate values to NA
+  # ex: 
+  return()
+}
+
+# for (i in 7:length(vis.list)) {
+#   if (i == 1) { # set max interval based on Vegetation Index to discard values outside of range
+#     maxVal <- 18
+#   } else if (i == 6) {
+#     maxVal <- 8} 
+#   else {
+#     maxVal <- 1
+#   }
