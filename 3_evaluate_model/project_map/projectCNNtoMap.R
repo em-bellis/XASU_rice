fa888463b7f872da3b7a5d96c9b5e0b1e25d3dd1
Emily Bellis <emilysbellis@gmail.com>
2021-03-15 10:43:47 -0500

    project CNN (on cluster)

--- 3_evaluate_model/project_map/projectCNNtoMap.R
+++ 3_evaluate_model/project_map/projectCNNtoMap.R
@@ -1,21 +1,12 @@
----
-title: "project_map_2DCNN"
-author: "EBellis"
-date: "3/3/2021"
-output: html_document
----
-This code will project a map of yield projections using model weights trained from a 2DCNN
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE, root.dir = '~/Documents/GitHub/XASU_rice/1_process_TIF_layers/')
-```
+# This code will project a map of yield projections using model weights trained from a 2DCNN
 
-## R code to process input image (downsample, mask many of the NA's, create raster stack)
-```{r}
+## process input image (downsample, mask many of the NA's, create raster stack)
 library(raster)
 library(rgdal)
 library(sf)
 library(stringr)
 library(ggplot2)
+library(reticulate)
 
 setwd("/home/ebellis/rice/project")
 source('get_max_val.R')
@@ -36,47 +27,50 @@ yld.5dm <- mask(yld.5d, crop_extent) # 5616 cells; 5235 of these are non-NA
 ## process all other layers (downsample, crop, mask); make raster stacks for each day; split into test, train, and validation sets; split each of these into 5x5 pixel subimages and then save each image and channel separately as .csv file 
 flydays <- c("04-11-2019","05-21-2019","06-13-2019","06-29-2019","07-11-2019","08-01-2019", "08-13-2019", "08-21-2019","08-28-2019","09-07-2019","09-13-2019") 
 channels <- c("CIgreen.tif","GNDVI.tif","NAVI.tif","NDVI.tif","RENDVI.tif","TGI.tif","Thermal.tif")
-path_to_raster <- "/Volumes/ABISSD/"
-vis.list <- str_replace(channels, '.tif', '')
 
-############################### output project for 1 day
-j = 6
-daystack <- create_daystack(path_to_raster, flydays[j], channels, yld.5dm) # raster stack for a single day
-  
+j = 6 # just looking at Aug 01
+daystack <- create_daystack(path_to, flydays[j], channels, yld.5dm) # raster stack for a single day
+
 # mask so all have same number of NA's and number of images is same
 mostnas_idx <- which.max(as.matrix(cellStats(daystack, stat='countNA')))
 yld.dayna <- mask(yld.5dm, daystack[[mostnas_idx]])
 daystack <- mask(daystack, daystack[[mostnas_idx]])
 
-```
-
-## Create a new raster with model predictions
-```{r}
 # create a new raster layer with extent of the yield layer, fill with NA's
 pred_yld <- yld.5dm
 values(pred_yld) <- NA
 pred_yld
-```
-# this is going to involve some python
-```{r}
-library(reticulate)
 
-#use_python('/Users/ebellis/Library/r-miniconda/envs/r-reticulate_rice/bin/python', required = T)
-use_condaenv("r-reticulate_rice", required = T)
-reticulate::py_config() # had to change .Renviron file on my system to get python version from conda env and load packages
-#repl_python()
+## stuff for python
+use_condaenv("rice_21", required = T)
+np <- import("numpy")
+sys <- import("sys")
 
-```
+library(tensorflow)
+library(keras)
+library(tfestimators)
 
-```{python}
-import numpy, sys
-import tensorflow.keras as keras
-model = keras.models.load_model('Aug01_SetB_model/')
-```
+# load model
+augmod <- load_model_tf('Aug01_SetB_model/', custom_objects = NULL, compile = TRUE)
 
-```{r}
-# get 5x5 section of image
-crop(daystack, extent(daystack, 500,505,500,505)) # crop using row and column numbers
+# a function that will take as input a 5x5 pixel region of predictors, and output yield, saving it in the prediction layer
+projectModel <- function(r, c) {
+  tmp <- as.array(crop(daystack, extent(daystack, r,(r+4),c,(c+4)))) # get a 5x5x7 array
+  tmp <- round(tmp,2) # just keep 2 decimal places
+  if (is.na(mean(tmp)) == "FALSE") {
+    patch <- matrix(predict(augmod, np$expand_dims(tmp, 0L)) %>% np$squeeze(), nrow = 5, byrow=T) # patch of raster with predictions
+  } else {
+    patch <- tmp[1:5,1:5,1]
+  }
+  return(patch)
+}
 
-```
+# starting at first indec
+for (r in seq(1,(dim(pred_yld)[1]-4),5)) {
+  for (c in seq(1,(dim(pred_yld)[2]-4),5)) {
+    pred_yld[r:(r+4), c:(c+4)] <- projectModel(r,c)
+  }
+}
+
+writeRaster(pred_yld, "projectCNN_Aug01.tif",format="GTiff")
 
---
+++ 3_evaluate_model/test.py
@@ -0,0 +1,7 @@
+def calcTreeDiameter(years): 
+    inches = 2 * years # growth rate of 2 in/year
+    print(inches)
+
+myTree = calcTreeDiameter(20)
+print(myTree)
+print(type(myTree))
