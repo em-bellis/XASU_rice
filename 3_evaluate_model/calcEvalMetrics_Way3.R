18957618d0853d4c6657a61aeae1f51107e1ff50
Emily Bellis <emilysbellis@gmail.com>
2021-08-30 12:02:38 -0500

    add way 3 for testing

--- 3_evaluate_model/calcEvalMetrics_Way3.R
+++ 3_evaluate_model/calcEvalMetrics_Way3.R
@@ -1,6 +1,3 @@
-##### The purpose of this script is to process remote sensing layers from the Hashem lab to create machine learning ready input files
-
-##### contact Emily Bellis (ebellis@astate.edu) with questions
 library(raster)
 library(rgdal)
 library(sf)
@@ -8,6 +5,8 @@ library(stringr)
 library(ggplot2)
 library(RColorBrewer)
 library(xgboost)
+library(tidyverse)
+library(wesanderson)
 
 setwd('~/Documents/GitHub/XASU_rice/1_process_TIF_layers/')
 path_to <- "/Volumes/LaCie/Dr.\ Reba/Humnok/Way_3/2020/07-05-2020/Index\ clips/"
@@ -49,110 +48,93 @@ yld.5dm <- mask(yld.5d, crop_extent) # 5616 cells; 5235 of these are non-NA
 ## process all other layers (downsample, crop, mask); make raster stacks for each day; split into test, train, and validation sets; split each of these into 5x5 pixel subimages and then save each image and channel separately as .csv file 
 channels <- c("CIgr_clip.tif","GNDVI_Clip.tif","NAVI_Clip.tif","../Raw\ raster\ clips/ndvi_Clip.tif","RENDVI_Clip.tif","TGI_Clip.tif","Thermal_Clip.tif")
 
-############################### output project for 1 day based on linear model
-cigr <- raster(paste0(path_to, channels[1]))
-cigr <- projectRaster(cigr, yld.5dm)
+for (i in 1:length(channels)) {
+  in_rast <- raster(paste0(path_to, channels[i]))
+  in_rast <- raster::calc(in_rast, fun=function(x){ x[x < 0] <- NA; return(x)} )  # 0 is minimum value
+  in_rast <- projectRaster(in_rast, yld.5dm)
+  in_rast <- mask(in_rast, yld.5dm)
+  
+  if(i == 1) {
+    daystack <- in_rast
+  } else {
+    daystack <- stack(daystack, in_rast)
+  }
+}
 
-tgi <- raster(paste0(path_to, channels[6]))
-tgi <- projectRaster(tgi, yld.5dm)
- 
+#writeRaster(daystack, filename = "Way3_070520_daystack")
+#writeRaster(yld.5dm, filename = "Way3_Yld_processed.tif", format = "GTiff")
+
+#####################
+# at this point, a raster layer of predictions is generated on the server, using the "Way3_070520_daystack" and "Way3_Yld_processed.tif" files
+#
+# now, load the trained models and generate predictions
+#####################
+
 # mask so all have same number of NA's and number of images is same
 mostnas_idx <- which.max(as.matrix(cellStats(daystack, stat='countNA')))
 yld.dayna <- mask(yld.5dm, daystack[[mostnas_idx]])
 daystack <- mask(daystack, daystack[[mostnas_idx]])
 daystack_yld <- stack(daystack, yld.dayna)
 
-preds_cnn <- raster(paste0('~/projectCNN_Aug01_Set',Set,'.tif'))
+preds_cnn <- raster('~/Desktop/Projects/rice_irrigation/projectCNN_070520_Way3.tif')
 preds_cnn <- extend(preds_cnn, 20)
 preds_cnn <- crop(preds_cnn, extent(daystack_yld))
 preds_cnn <- mask(preds_cnn, daystack[[mostnas_idx]])
 names(preds_cnn) <- 'preds_2d'
 daystack_yld <- stack(daystack_yld, preds_cnn)
 
-# split into test, validation, training sets
-train_yld <- crop(daystack_yld, e.train)
-test_yld <- crop(daystack_yld, e.test)
+way3df <- as.data.frame(na.omit(as.matrix(daystack_yld))) # drop any rows with NA for any vegetation index
+colnames(way3df) <- colnames(test.df)[1:8]
+colnames(way3df)[9] <- 'preds_2d'
 
-# create a data frame for each set 
-train.df <- as.data.frame(na.omit(as.matrix(train_yld))) # drop any rows with NA for any vegetation index
-test.df <- as.data.frame(na.omit(as.matrix(test_yld)))
+# add a column for predictions
+way3df$Yield_Mgha <- buac_to_Mgha(way3df$Yield)
+way3df$preds_lin <- buac_to_Mgha(predict(mod.lin, way3df))
+way3df$preds_null <- buac_to_Mgha(mod.null)
 
-test.df$Yield_Mgha <- buac_to_Mgha(test.df$Yield)
+way3df$preds_2d <- buac_to_Mgha(way3df$preds_2d)
 
-# add a column for predictions for each model
-mod.lin = lm(Yield ~ CIgreen + GNDVI + NAVI + NDVI + RENDVI + TGI + Thermal, data = train.df)
-test.df$preds_lin <- buac_to_Mgha(predict(mod.lin, test.df))
-
-mod.null = mean(train.df$Yield)
-test.df$preds_null <- buac_to_Mgha(mod.null)
-
-test.df$preds_2d <- buac_to_Mgha(test.df$preds_2d)
-
-## xgboost model (did not save, so retrain)
-dtrain <- xgb.DMatrix(data = as.matrix(train.df[,c(1:7)]), label = as.matrix(train.df[,8]))
-dtest <- xgb.DMatrix(data = as.matrix(test.df[,c(1:7)]), label = as.matrix(test.df[,8]))
-rownames(dtest) <- NULL
-bst <- xgboost(data = dtrain, nrounds=200, max.depth=2, verbose=2, eta = 0.2)
-test.df$preds_xgb = buac_to_Mgha(predict(bst, dtest))
+dtest <- xgb.DMatrix(data = as.matrix(way3df[,c(1:7)]), label = as.matrix(way3df[,8]))
+way3df$preds_xgb = buac_to_Mgha(predict(bst, dtest))
 
 # calculate metrics
 # rmse
-rmse(test.df$Yield_Mgha, test.df$preds_2d)
-rmse(test.df$Yield_Mgha, test.df$preds_xgb)
-rmse(test.df$Yield_Mgha, test.df$preds_lin)
-rmse(test.df$Yield_Mgha, test.df$preds_null)
+rmse(way3df$Yield_Mgha, way3df$preds_2d)
+rmse(way3df$Yield_Mgha, way3df$preds_xgb)
+rmse(way3df$Yield_Mgha, way3df$preds_lin)
+rmse(way3df$Yield_Mgha, way3df$preds_null)
 
 #r2
-r2(test.df$Yield_Mgha, test.df$preds_2d)
-r2(test.df$Yield_Mgha, test.df$preds_xgb)
-r2(test.df$Yield_Mgha, test.df$preds_lin)
-r2(test.df$Yield_Mgha, test.df$preds_null)
+r2(way3df$Yield_Mgha, way3df$preds_2d)
+r2(way3df$Yield_Mgha, way3df$preds_xgb)
+r2(way3df$Yield_Mgha, way3df$preds_lin)
+r2(way3df$Yield_Mgha, way3df$preds_null)
 
 #mbe
-mbe(test.df$Yield_Mgha, test.df$preds_2d)
-mbe(test.df$Yield_Mgha, test.df$preds_xgb)
-mbe(test.df$Yield_Mgha, test.df$preds_lin)
-mbe(test.df$Yield_Mgha, test.df$preds_null)
+mbe(way3df$Yield_Mgha, way3df$preds_2d)
+mbe(way3df$Yield_Mgha, way3df$preds_xgb)
+mbe(way3df$Yield_Mgha, way3df$preds_lin)
+mbe(way3df$Yield_Mgha, way3df$preds_null)
 
 #mae
-mae(test.df$Yield_Mgha, test.df$preds_2d)
-mae(test.df$Yield_Mgha, test.df$preds_xgb)
-mae(test.df$Yield_Mgha, test.df$preds_lin)
-mae(test.df$Yield_Mgha, test.df$preds_null)
+mae(way3df$Yield_Mgha, way3df$preds_2d)
+mae(way3df$Yield_Mgha, way3df$preds_xgb)
+mae(way3df$Yield_Mgha, way3df$preds_lin)
+mae(way3df$Yield_Mgha, way3df$preds_null)
 
+## figure
+all <- read.csv('../Figures_for_MS/setb_way3_070520.csv', header = T)
+all <- all %>% 
+  pivot_longer(cols = -model)
 
+p <- ggplot(all, aes(x = name, y = value, fill = model)) +
+  geom_bar(stat = "identity", position = "dodge", col = "black", lwd = 0.2) +
+  theme_light() +
+  theme(panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = wes_palette("Zissou1", 5, type = c("discrete"))[c(1,3,5)], 
+                    name = "Model") +
+  xlab("")
 
-####### 3dcnn is calculated based on predictions output from model.predict run on cluster
-setA <- read.csv('preds_3dcnn_SetA.csv')
-setB <- read.csv('preds_3dcnn_SetB.csv')
-setC <- read.csv('preds_3dcnn_SetC.csv')
-setD <- read.csv('preds_3dcnn_SetD.csv')
-
-#mae
-mae(buac_to_Mgha(setA$observed), buac_to_Mgha(setA$predicted))
-mae(buac_to_Mgha(setB$observed), buac_to_Mgha(setB$predicted))
-mae(buac_to_Mgha(setC$observed), buac_to_Mgha(setC$predicted))
-mae(buac_to_Mgha(setD$observed), buac_to_Mgha(setD$predicted))
-
-#mbe
-mbe(buac_to_Mgha(setA$observed), buac_to_Mgha(setA$predicted))
-mbe(buac_to_Mgha(setB$observed), buac_to_Mgha(setB$predicted))
-mbe(buac_to_Mgha(setC$observed), buac_to_Mgha(setC$predicted))
-mbe(buac_to_Mgha(setD$observed), buac_to_Mgha(setD$predicted))
-
-#r2
-r2(buac_to_Mgha(setA$observed), buac_to_Mgha(setA$predicted))
-r2(buac_to_Mgha(setB$observed), buac_to_Mgha(setB$predicted))
-r2(buac_to_Mgha(setC$observed), buac_to_Mgha(setC$predicted))
-r2(buac_to_Mgha(setD$observed), buac_to_Mgha(setD$predicted))
-
-#rmse
-rmse(buac_to_Mgha(setA$observed), buac_to_Mgha(setA$predicted))
-rmse(buac_to_Mgha(setB$observed), buac_to_Mgha(setB$predicted))
-rmse(buac_to_Mgha(setC$observed), buac_to_Mgha(setC$predicted))
-rmse(buac_to_Mgha(setD$observed), buac_to_Mgha(setD$predicted))
-
-####### stats
-results <- read.csv('../Figures_for_MS/results_summary.csv', header = T)
-library(lme4)
-glm
+pdf('../Figures_for_MS/Figure6.pdf', height = 2.5, width = 3)
+p
+dev.off()
